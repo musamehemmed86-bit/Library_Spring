0c2243661128533303506aecee678854a87d52e8
musamehemmed86-bit <musamehemmed86@gmail.com>
2026-01-28 19:44:56 +0400

    Update LibraryApplication.java

--- library/src/main/java/com/library/dea/LibraryApplication.java
+++ library/src/main/java/com/library/dea/LibraryApplication.java
@@ -2,12 +2,24 @@ package com.library.dea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class LibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+
+//		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class); // bean storage
+//
+//		Book book = context.getBean(Book.class);
+//
+//		book.setTitle("Java");
+//		book.setAuthor("Games Gosling");
+//
+//		System.out.println(book.getTitle());
+//		System.out.println(book.getAuthor());
+    }
 
 }
